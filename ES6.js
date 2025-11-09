54d085695b7909d4cc17d4cf4ba649228743ab7d
Kalpana Air <ayerkasak@gmail.com>
2025-11-09 14:30:45 +0545

    learn about function, spread and rest operator

---
+++ ES6.js
@@ -0,0 +1,475 @@
+// let personinArray = ["Kasak", 28, false]
+
+// let person ={
+//     name: "Kasak",
+//     age: 28,
+//     isMarried: false,
+//     gender: "FEMALE",
+//     school: {
+//         name: "Stepping",
+//         class: 10,
+
+//     },
+// }
+
+// //spread/rest oprator => ... , used in array and object
+
+// function sumOfEvenNumbers(numbers){
+//     let sum = 0;
+//     for( let i = 1;  i<= numbers.length - 1; i++){
+//         if (numbers[i] % 2 == 0){
+//             sum = sum + i;
+//         }                   
+//     }
+//     return sum
+// }
+
+// function sumOfNNaturalNumbers(){
+//     let N = Number (prompt("Enter a number:"));
+//     let sum = 0;
+//     for (let i = 1; i<=N; i++){
+//         sum = sum + i;
+//     }
+//     return sum
+// }
+
+// let numbers = [1,2,3,4]
+
+// function calculateSum(firstNum, secondNum, ...nums){
+//     console.log({
+//         firstNum, secondNum, nums
+//     })
+//     console.log(firstNum, secondNum, nums);
+//     console.log("Firstnum:" + firstNum, "secondNum:" + secondNum, "nums:" + nums);
+//     let sum = 0;
+//     for(let i = 0; i <= nums.length - 1; i++){
+//         sum = sum + nums[i]
+    
+//     }
+//     return sum
+    
+// }
+
+// // console.log(calculateSum(numbers));
+// console.log(calculateSum(1,2,3,4));
+    
+
+// //rest in object
+// var students ={
+//     name: "Kasak",
+//     age: 28,
+//     hobbies: ["reading", "watching movies", "listening music"]
+// }
+// // const age = students.age
+// // const {age} = students;
+// // const {age, ...rest} = students;
+// const{...rest} = students;
+// console.log(rest)
+
+
+// // spread in object
+// var newStudent = {
+//     ...students,
+//     age: 29
+// }
+// console.log(newStudent)
+
+// // rest in array
+// function addNumbers(a,b,c, ...others){
+//     console.log(others)
+//     return a+b+c
+    
+// }
+// const res = addNumbers(2,3,4,5,6,7,8,9,90)
+// console.log(res)
+
+// // spread in array
+// var names = ["Kasak", "Indu", "Piku", "Gitu"];
+// function getName(name1,name2,name3){
+//     console.log(name1, name2, name3)
+// }
+// getName(...names)
+
+// // Question 1
+// function sumNumbers(a,b,c){
+//     return a + b + c
+// }
+
+// const numb = [2,3,4,5,6,7,];
+// console.log(...numb)
+// console.log(sumNumbers(...numb))
+// answer by chatgpt for CountQueuingStrategy. 1
+// function sumNumbers(...numbers) {
+//   let sum = 0;
+//   for (let num of numbers) {
+//     sum += num;
+//   }
+//   return sum;
+// }
+
+// const numb = [2, 3, 4, 5, 6, 7];
+// console.log(sumNumbers(...numb)); // Output: 27
+// question no. 2
+// let obj = {
+//     firstName: "Kasak",
+//     lastName: "Air",
+//     hobbies : ["reading", "coding", "music"]
+// }
+// console.log(`Hello, my name is ${obj.firstName} ${obj.lastName}`)
+// console.log(`My hobbies are ${obj.hobbies.join("\n")}`)
+
+// function introduce(firstName, lastName, ...hobbies){
+
+//     console.log(`Hello, my name is ${firstName} ${lastName}`)
+//     console.log(`My hobbies are ${hobbies.join(", ")}`)
+    
+
+
+// }
+// introduce("Kalpana", "Air", "reading", "coding", "music");
+
+
+// const fruits = ["apple", "banana", "orange"];
+
+// function findMax(...numbers){
+//     console.log(Math.max(...numbers))
+// }
+// findMax(1,5,7,2,9)
+
+// const num = [1, 2, 3];
+// const num2 = [4, 5, 6];
+
+// function mergeAndDouble(a, b, ...c){//rest oprator
+//     let num3 = [...a, ...b]// spread operator    // console.log(num3)
+//     console.log(...c)//[[1], [2,4]]
+    // let doubleNum = []
+    // for (let i = 0; i <= num3.length -1 ; i++){
+    //     let num4 = num3[i] * 2;
+    //     doubleNum.push(num4)
+
+    // }
+
+    // num3.forEach(function(value, ...rest){
+    //     console.log({value, rest})
+    //     let num4 = value * 2;    
+    //     doubleNum.push(num4)
+    // })
+
+    
+//     let doubleNum = num3.map(function(value, ...rest){
+//         console.log({value, rest})
+//         let num4 = value * 2;
+//         return num4
+//     })
+//     return doubleNum
+// }
+// console.log(mergeAndDouble(num, num2, true))
+
+// let fruits = ["apple", "banana", "mango"]
+// function arrFruits() {
+//     fruits.push("orange");
+//     fruits.pop();
+// }
+// arrFruits();
+
+// console.log(fruits);
+
+
+// let colors = ["red", "green", "blue"];
+// function arrColors(){
+//     colors.unshift("yellow")
+//     // colors.shift()
+
+// }
+// arrColors();
+
+// console.log(colors)
+
+// let newNumbers = [1,2,3,4]
+
+// function double(n){
+//     return n * 2
+// }
+// let newArr = newNumbers.map(double);
+// // console.log(newArr)
+
+// let numbs = [1, 2, 3, 4, 5, 6, 7, 8]
+// let even = numbs.filter((n) => {
+//    const isEven = n % 2 == 0
+//    if(isEven) {
+//     console.log(n)
+//    }
+//    return isEven
+// });
+// console.log(even)
+
+// let sum = numbs.reduce((acc, curr) => {
+//     return acc + curr;
+// }, 0);
+// console.log(sum)
+// console.log({beforSclice: numbs})
+
+// let newNumb = numbs.slice(2,5)
+// console.log({afterSclice: numbs, newNumb})
+
+// console.log(newNumb) //output: [3, 4, 5]
+
+// let newNumber = numbs.splice(1,4)
+
+// let a = [2, 4, 6, 8]
+// let sum = a.reduce((acc, curr) => {
+//     return acc + curr;
+// }, 0);
+// console.log(sum)
+
+// let pro = a.reduce((acc, curr) => {
+//     return acc * curr
+// }, 1)
+// console.log(`The product is ${pro}`);
+
+// // let colors = ["black", "white", "olive green"];
+// // colors.forEach((val, index) => {
+// //     console.log(index + ":" + val);
+// // });
+
+// let b = [2, 3, 4, 5]
+// let sqr = b.map(n => n * n)
+//     console.log(sqr);
+
+// //function and its types
+// //function with name
+// function sayHello(name) {
+//     return "Hello " + name 
+// }
+
+
+
+// //ananomous function 
+// function (name) {
+//     return "Hello " + name 
+// }
+// //we can't call it but it can be passed as arguments on some other function
+
+// //arrow function and its ways of writing on different conditions
+// const sayBye = (name) => {
+//     return "Bye! " + name;
+// }
+
+// //ananomous arrow function
+// (name) => {
+//     return "Bye! " + name;
+// }
+// //but when we have single line of code inside the function block and if that is return statement we can do:
+// const _sayBye = (name) => "Bye! " + name  
+
+// const _sayByeConsole = (name) => "Bye! " + name
+
+// const sayByeConsole = (name) => {
+//     let con = console.log("Bye! " + name)
+//     return "Bye! " + name
+// }  
+
+
+// let b = [2, 3, 4, 5]
+// let sqr = b.map(function (num) {
+//     return num * num 
+// })
+// console.log(sqr);
+
+
+// function square(num) {
+//     return num * num
+// }
+// let _sqr = b.map(square)
+// console.log(_sqr);
+
+// myMap(a) {
+//     let name = "kasak"
+//     a(name)
+// }
+// myMap(square)
+// myMap(function (name){
+//     console.log("helloworld", name)
+// })
+// myMap((name) => {
+//     console.log("helloworld", name)
+// })
+// myMap(name => console.log("helloworld", name)
+// )
+
+// myMap((_name) => {
+//     console.log(_name)
+// })
+
+// const newArr = name_ => console.log(name_)
+// myMap(newArr)
+
+// Q1. Basic function call
+
+// Write a function outer() that defines another function inner().
+// When you call outer(), it should call inner() and print "Hello from inner function!".
+
+function outer(){
+    function inner() {
+        console.log("Hello from inner function!")
+    }
+    inner()
+}
+
+outer()
+
+// Q2. Passing argument to inner function
+
+// Create a function greet(name) that defines another function sayHi() inside it.
+// The sayHi() function should print:
+// 👉 "Hi, " + name.
+
+function greet(name){
+    function sayHi(){
+        return "Hi, " + name
+    }
+    return sayHi()
+}
+console.log(greet("Kasak"));
+
+// Q3. Returning value from inner function
+// Write a function outer(num) that defines an inner function square(x) which returns x * x.
+// outer() should call square(num) and return its result.
+
+function outerFunc(num){
+    function square(x){
+        return x * x
+    }
+    return square(3);
+
+}
+const newSqr = outerFunc()
+console.log(newSqr)
+
+// Q4. Inner function accessing outer variable (closure)
+// Make a function outer() that defines a variable count = 5 and an inner function showCount() which prints count.
+// Call showCount() inside outer().
+
+function newOuter(){
+    let count = 5;
+    function showCount(){
+        console.log(count);
+    }
+    showCount();
+}
+newOuter();
+
+// Q5. Passing function as a parameter
+// Write a function operate(a, b, fn) that takes a function fn as an argument and uses it to combine the two numbers.
+function operate(a,b){
+    return a + b
+}
+function newOperate(func, val, val1){
+    console.log(func(val, val1));
+}
+
+newOperate(operate, 2, 3)
+
+// Q6. Returning a function
+// Write a function multiplyBy(factor) that returns another function which multiplies any given number by factor.
+ 
+function multiplyBy(factor){
+    return function(number){
+        return factor * number
+    };
+}
+
+const double = multiplyBy(2);
+const triple = multiplyBy(3);
+
+console.log(double(5));
+console.log(triple(5));
+
+// Q7. Nested function chain
+// Write a function outer() that defines a function middle(), which defines another function inner().
+// When outer() is called, all three functions should print messages in order.
+function nOuter(){
+    console.log("Hello from Outer.");
+    function middle(){
+        console.log("Hello from middle.");
+        function newInner(){
+            console.log("Hello from Inner.");
+        }
+        newInner();
+
+        }
+    middle();
+    }
+nOuter();
+    
+// Q8. Outer and inner both take arguments
+// Write a function mathOperation(a, b) that defines an inner function add(x, y) that returns their sum.
+// mathOperation() should return the result of add(a, b).
+
+function mathOperation(a, b){
+    function add(x, y){
+        return x + y
+    }
+    return add(a, b)
+}
+
+const newSum = mathOperation(2,3);
+console.log(newSum)
+
+// about spread and rest operator
+function sumNew(...numbers) {
+
+  return numbers.reduce((a, b) => a + b, 0);
+}
+console.log(sumNew(1, 2, 3)); 
+
+
+const [first, second, ...rest] = [10, 20, 30, 40, 50];
+console.log(first, second); 
+console.log(rest); 
+
+
+const obj = { a: 1, b: 2, c: 3 };
+const { a, ...others } = obj;
+console.log(a);      
+console.log(others); 
+
+const app = ('gyanchand');
+console.log(app)
+
+
+// Example: rest in function
+function joinWith(separator, ...words) {
+  return words.join(separator);
+}
+console.log(joinWith("*", "a", "b", "c")); // "a-b-c"
+
+
+// Write a function called calculateAverage() that takes this array as an argument and returns the average of the numbers.
+
+const numbers = [5, 10, 15, 20, 25];
+
+function calculateAverage(arr){
+    let totalSum = numbers.reduce((a, b) =>  a + b, 0);
+    return totalSum / arr.length
+}
+console.log(calculateAverage(numbers))
+
+// Write a function called getTopStudents() that:
+// Takes the array of students as an argument.
+// Filters out all students who scored below 80 marks.
+// Returns a new array containing only the names of those top students.
+// Bonus challenge:
+// Use arrow functions and array methods (filter() + map()).
+
+const students = [
+  { name: "Aarav", marks: 85 },
+  { name: "Priya", marks: 92 },
+  { name: "Rahul", marks: 76 },
+  { name: "Sneha", marks: 64 },
+  { name: "Kabir", marks: 88 }
+];
+
+const topStudents = students.filter(student => student.marks >= 80);
+const topStudentsNames = topStudents.map(students => students.name);
+console.log(topStudentsNames);
--- script.js
+++ script.js
@@ -1,93 +1,147 @@
-let currentPlayer = "X";
-let arr = Array(9).fill(null);
-let isGameCompleted = false; 
+let currentPlayer = localStorage.getItem("currentPlayer") || "X";
+let arr = JSON.parse(localStorage.getItem("arr")) || Array(9).fill(null);
+let isGameCompleted = JSON.parse(localStorage.getItem("isGameCompleted")) || false;
 
-function hasAnyoneWon (){
-    let status =  (arr[0] !== null && arr[0] === arr[1] && arr[1] === arr[2]) ||
-    (arr[3] !== null && arr[3] === arr[4] && arr[4] === arr[5]) ||
-    (arr[6] !== null && arr[6] === arr[7] && arr[7] === arr[8]) ||
-    (arr[0] !== null && arr[0] === arr[3] && arr[3] === arr[6]) ||
-    (arr[1] !== null && arr[1] === arr[4] && arr[4] === arr[7]) ||
-    (arr[2] !== null && arr[2] === arr[5] && arr[5] === arr[8]) ||
-    (arr[0] !== null && arr[0] === arr[4] && arr[4] === arr[8]) ||
-    (arr[2] !== null && arr[2] === arr[4] && arr[4] === arr[6]) 
-
-    return status
-} 
-
-
-function checkWinner(){
-    if(
-        hasAnyoneWon()
-    ){
-    showResultofTheMatch(`The Winner is ${currentPlayer}`)
+// Initialize the board when page loads
+window.addEventListener('DOMContentLoaded', function() {
+    renderBoardFromStorage();
+    if (isGameCompleted) {
+        showStoredResult();
     }
-    
+});
+
+function hasAnyoneWon() {
+    let status = (arr[0] !== null && arr[0] === arr[1] && arr[1] === arr[2]) ||
+                 (arr[3] !== null && arr[3] === arr[4] && arr[4] === arr[5]) ||
+                 (arr[6] !== null && arr[6] === arr[7] && arr[7] === arr[8]) ||
+                 (arr[0] !== null && arr[0] === arr[3] && arr[3] === arr[6]) ||
+                 (arr[1] !== null && arr[1] === arr[4] && arr[4] === arr[7]) ||
+                 (arr[2] !== null && arr[2] === arr[5] && arr[5] === arr[8]) ||
+                 (arr[0] !== null && arr[0] === arr[4] && arr[4] === arr[8]) ||
+                 (arr[2] !== null && arr[2] === arr[4] && arr[4] === arr[6]);
+    return status;
 }
 
-function handleClick(el){
-    const id = Number(el.id);
-    if (arr[id] || isGameCompleted){
-        return
+function checkWinner() {
+    if (hasAnyoneWon()) {
+        showResultofTheMatch(`The Winner is ${currentPlayer}`);
+        // Update localStorage when game is completed
+        localStorage.setItem("isGameCompleted", JSON.stringify(true));
     }
+}
+
+function handleClick(el) {
+    const id = Number(el.id);
+    if (arr[id] || isGameCompleted) {
+        return;
+    }
+    
     arr[id] = currentPlayer;
     el.innerText = currentPlayer;
+    
+    // Update localStorage after each move
+    localStorage.setItem("arr", JSON.stringify(arr));
+    
     checkWinner();
     checkDraw();
-    if (isGameCompleted){
-        return
+    
+    if (isGameCompleted) {
+        return;
     }
+    
+    // Switch player and update localStorage
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    
-   
+    localStorage.setItem("currentPlayer", currentPlayer);
 }
 
-function restartGame(){
- arr = Array(9).fill(null);
-//  currentPlayer = "X"
- isGameCompleted = false;
- const toResetColumns = document.querySelectorAll('.col');
- for (let i = 0; i < toResetColumns.length; i++) {
-    const element = toResetColumns[i];
-   element.innerHTML = "";
-   
- }
- const resetDiv = document.querySelector('.resetDiv');
- resetDiv.remove();
-}
-
-function checkDraw(){
-    const areBoxesFulfilled = arr.every((element)=> {
-            return element !== null;
-         })
-     if( 
-        areBoxesFulfilled && !hasAnyoneWon()
-    ){
-        showResultofTheMatch("Draw !" )
+function restartGame() {
+    // Reset game state
+    arr = Array(9).fill(null);
+    currentPlayer = "X";
+    isGameCompleted = false;
     
+    // Clear localStorage
+    localStorage.setItem("arr", JSON.stringify(arr));
+    localStorage.setItem("currentPlayer", currentPlayer);
+    localStorage.setItem("isGameCompleted", JSON.stringify(false));
+    
+    // Reset UI
+    const toResetColumns = document.querySelectorAll('.col');
+    for (let i = 0; i < toResetColumns.length; i++) {
+        const element = toResetColumns[i];
+        element.innerText = "";
+    }
+    
+    const resetDiv = document.querySelector('.resetDiv');
+    if (resetDiv) {
+        resetDiv.remove();
     }
 }
 
-function showResultofTheMatch(pText){
+function checkDraw() {
+    const areBoxesFulfilled = arr.every((element) => element !== null);
+    if (areBoxesFulfilled && !hasAnyoneWon()) {
+        showResultofTheMatch("Draw !");
+        // Update localStorage when game is completed
+        localStorage.setItem("isGameCompleted", JSON.stringify(true));
+    }
+}
+
+function showResultofTheMatch(pText) {
     const container = document.querySelector('.container');
     const newDiv = document.createElement('div');
     newDiv.className = "resetDiv";
     const newP = document.createElement('p');
     const restartButton = document.createElement('button');
+    
     restartButton.textContent = "Restart Game";
     restartButton.addEventListener('click', restartGame);
-    newP.textContent = pText
-    // if (hasAnyoneWon()){
-    //     newP.textContent = `The Winner is ${currentPlayer}`
-    // } else{
-    //     newP.textContent = `Draw!`
-    //     }
-
-   
+    newP.textContent = pText;
+    
     newDiv.appendChild(newP);
     newDiv.appendChild(restartButton);
-    container.insertAdjacentElement('afterend', newDiv);
+    
     const gameWrapper = document.querySelector('.game-wrapper');
     gameWrapper.appendChild(newDiv);
+    
     isGameCompleted = true;
+    // Update localStorage when game is completed
+    localStorage.setItem("isGameCompleted", JSON.stringify(true));
+}
+
+// New function to render board from localStorage when page loads
+const fun = (cell, index) => {
+console.log(cell, index)
+}
+// fun("5th", 4)
+function renderBoardFromStorage() {
+    const cells = document.querySelectorAll('.col');
+    // for (let i = 0; i < array.length; i++) {
+    //     const element = array[i];
+
+    //     fun()
+        
+    // }
+    
+    cells.forEach(()=>{
+
+    })
+    cells.forEach((cell, index) => {
+        if (arr[index] !== null) {
+            cell.innerText = arr[index];
+        }
+    });
+}
+
+// New function to show stored result when page reloads
+function showStoredResult() {
+    if (hasAnyoneWon()) {
+        showResultofTheMatch(`The Winner is ${currentPlayer}`);
+    } else {
+        // Check if it's a draw (all cells filled but no winner)
+        const areBoxesFulfilled = arr.every((element) => element !== null);
+        if (areBoxesFulfilled) {
+            showResultofTheMatch("Draw !");
+        }
+    }
 }
