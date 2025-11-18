1a53ee3a66ab8e7c0733120dd5c9a8df97173625
Kalpana Air <ayerkasak@gmail.com>
2025-11-18 23:22:49 +0545

    Practice Task 3 for DOM change style

--- DOM_Practice/script.js
+++ DOM_Practice/script.js
@@ -2,6 +2,9 @@ const head = document.getElementById("heading");
 const button = document.getElementById("btn");
 const msg = document.getElementById("msg")
 msg.textContent = "New Text"
+msg.style.color = "red";
+msg.style.fontSize = "30px";
+msg.style.backgroundColor = "yellow";
 
 console.log(head);
 console.log(button);
