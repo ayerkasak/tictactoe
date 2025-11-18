4b5d8be730e123244823cdbadec6162688d11f0e
Kalpana Air <ayerkasak@gmail.com>
2025-11-18 23:19:37 +0545

    Practice Task 2 for DOM .textContent

--- DOM_Practice/script.js
+++ DOM_Practice/script.js
@@ -1,5 +1,7 @@
 const head = document.getElementById("heading");
 const button = document.getElementById("btn");
+const msg = document.getElementById("msg")
+msg.textContent = "New Text"
 
 console.log(head);
 console.log(button);
