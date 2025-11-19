54efd92ed313b7abce32621a158338524ca8c059
Kalpana Air <ayerkasak@gmail.com>
2025-11-19 21:49:45 +0545

    Practice of addEventListener on Button

--- DOM_Practice/script.js
+++ DOM_Practice/script.js
@@ -1,12 +1,27 @@
-const head = document.getElementById("heading");
-const button = document.getElementById("btn");
-const msg = document.getElementById("msg")
-msg.textContent = "New Text"
-msg.style.color = "red";
-msg.style.fontSize = "30px";
-msg.style.backgroundColor = "yellow";
+// const head = document.getElementById("heading");
+// const button = document.getElementById("btn");
+// const msg = document.getElementById("msg")
+// msg.textContent = "New Text"
+// msg.style.color = "red";
+// msg.style.fontSize = "30px";
+// msg.style.backgroundColor = "yellow";
 
-document.getElementById("myImage").src = "tree.jpg"
 
-console.log(head);
-console.log(button);
+// document.getElementById("myImage").src = "tree.jpg"
+
+// console.log(head);
+// console.log(button);
+
+
+const catButton = document.getElementById("showCatBtn");
+const dogButton = document.getElementById("showDogBtn");
+
+catButton.addEventListener("click", function(){
+    alert("You clicked the cat Button!");
+});
+
+dogButton.addEventListener("click", function(){
+    alert("You clicked Dog Button!");
+});
+
+
