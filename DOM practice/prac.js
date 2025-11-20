76a63b6715767100e5910341372ea68dd376f685
Kalpana Air <ayerkasak@gmail.com>
2025-11-20 11:12:12 +0545

    Calculate sum of numbers using array - Task 1

--- DOM practice/prac.js
+++ DOM practice/prac.js
@@ -1,25 +1,25 @@
-const parents = [
-    {name: "Aanad", phone: 465789, email:"aanand.b@gmail.com", children:[
-        {name:"Aanandi", school:"Stepping", class:1},
-        {name:"Andy", school:"Stepping", class:3},
-        {name:"Ann", school:"Stepping", class:4}
+// const parents = [
+//     {name: "Aanad", phone: 465789, email:"aanand.b@gmail.com", children:[
+//         {name:"Aanandi", school:"Stepping", class:1},
+//         {name:"Andy", school:"Stepping", class:3},
+//         {name:"Ann", school:"Stepping", class:4}
       
-    ]},
+//     ]},
 
-     {name: "Khushbu", phone: 46533789, email:"khushbu.b@gmail.com", children:[
-        {name:"Adyant", school:"hope", class:1},
-        {name:"Gyanu", school:"Stepping", class:3},
-        {name:"Nanu", school:"Stepping", class:4}
+//      {name: "Khushbu", phone: 46533789, email:"khushbu.b@gmail.com", children:[
+//         {name:"Adyant", school:"hope", class:1},
+//         {name:"Gyanu", school:"Stepping", class:3},
+//         {name:"Nanu", school:"Stepping", class:4}
       
-    ]},
+//     ]},
 
-     {name: "Kasak", phone: 465789, email:"aanand.b@gmail.com", children:[
-        {name:"Avika", school:"Stepping", class:1},
-        {name:"Rudra", school:"Stepping", class:3},
-        {name:"Reet", school:"Stepping", class:4}
+//      {name: "Kasak", phone: 465789, email:"aanand.b@gmail.com", children:[
+//         {name:"Avika", school:"Stepping", class:1},
+//         {name:"Rudra", school:"Stepping", class:3},
+//         {name:"Reet", school:"Stepping", class:4}
       
-    ]}
-]
+//     ]}
+// ]
 // function totalNumberOfChild(){
 //     let totalChildren = 0;
 //     for(let i = 0; i < parents.length; i++){
@@ -39,11 +39,11 @@ const parents = [
 // }
 // console.log(totalNumberOfChild());
 
-const totalChildren = parents.reduce((acc, curr) =>{
-    return acc + curr.children.length
+// const totalChildren = parents.reduce((acc, curr) =>{
+//     return acc + curr.children.length
     
-},0);
-console.log(totalChildren);
+// },0);
+// console.log(totalChildren);
 
 
 // yeuta function create garne that returns the total number of child inside the array
@@ -53,3 +53,12 @@ console.log(totalChildren);
 // chidrenxa bhane, childrenko array lengthlai particular variable add gardai store garne,
 // for loop bahira chahi variable return garxa function bata
 // at last print function output 
+
+// Question 1 — Beginner Level - Given an array of numbers, use reduce() to find the sum of all numbers in the array.
+
+const numbers = [3, 7, 2, 9, 5];
+const sumOfNumbers = numbers.reduce((acc, curr) => {
+    return acc + curr
+}, 0)
+
+console.log(sumOfNumbers);
