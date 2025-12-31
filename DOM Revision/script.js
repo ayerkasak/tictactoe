e071457f198db93e3eea45947b45ae3d325fb1e7
Kalpana Air <ayerkasak@gmail.com>
2025-12-31 14:57:58 +0545

    DOM Revision q 2

--- DOM Revision/script.js
+++ DOM Revision/script.js
@@ -1,6 +1,24 @@
-const p = document.getElementById("text");
-const btn = document.getElementById("btn");
+// const p = document.getElementById("text");
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener('click', function(){
+//     p.innerText = "DOM is Fun."
+// });
+
+const inputItem = document.getElementById("itemInput");
+const btn = document.getElementById("addBtn");
+const list = document.getElementById("itemList");
 
 btn.addEventListener('click', function(){
-    p.innerText = "DOM is Fun."
-});
+    if(inputItem.value === ""){
+        alert("Please enter an item.");
+        return;
+    }
+    const li = document.createElement("li");
+    li.innerText = inputItem.value;
+
+    list.appendChild(li);
+    
+    inputItem.value = "";
+
+})
