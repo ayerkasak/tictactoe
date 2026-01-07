c8b99eb47aff33e3c14a50da209160c37c1c22ad
Kalpana Air <ayerkasak@gmail.com>
2026-01-07 14:53:08 +0545

    Tenth pattern

--- DOM Revision/star_pattern.js
+++ DOM Revision/star_pattern.js
@@ -150,20 +150,49 @@
 
 // Ninth pattern
 
+// let alphabets = "abcdef";
+// for(let i = 1; i <= 4; i++){
+//     let row = "";
+//     let k = 1;
+//     for(let j = 1; j <= 7; j++){
+//         if(j >= 5-i && j <= 3 + i){
+//            row += alphabets[k-1]
+//            if(j<4){
+//             k++
+//            }else{
+//             k--;
+//            }
+//         }else{
+//             row += " ";
+//         }
+//     }
+//     console.log(row);
+// }
+
+// Tenth Pattern
+// abcdba
+// abc dcb
+// ab   cb
+// a     b
+
 let alphabets = "abcdef";
 for(let i = 1; i <= 4; i++){
     let row = "";
     let k = 1;
     for(let j = 1; j <= 7; j++){
-        if(j >= 5-i && j <= 3 + i){
-           row += alphabets[k-1]
-           if(j<4){
-            k++
-           }else{
-            k--;
-           }
-        }else{
+        if(j <= 5-i || j >= 3+i){
+            row += alphabets[k-1];
+            if(j<4){
+                k++;
+            } else{
+                k--;
+            }
+        }
+        else{
             row += " ";
+            if(j == 4){
+                k--;
+            }
         }
     }
     console.log(row);
