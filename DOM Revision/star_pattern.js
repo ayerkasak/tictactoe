ffc439b615faadf5626c80ac2bf3eb0bc8e8fe54
Kalpana Air <ayerkasak@gmail.com>
2026-01-07 21:31:58 +0545

    Eleventh Star Pattern

--- DOM Revision/star_pattern.js
+++ DOM Revision/star_pattern.js
@@ -175,25 +175,45 @@
 // ab   cb
 // a     b
 
-let alphabets = "abcdef";
-for(let i = 1; i <= 4; i++){
+// let alphabets = "abcdef";
+// for(let i = 1; i <= 4; i++){
+//     let row = "";
+//     let k = 1;
+//     for(let j = 1; j <= 7; j++){
+//         if(j <= 5-i || j >= 3+i){
+//             row += alphabets[k-1];
+//             if(j<4){
+//                 k++;
+//             } else{
+//                 k--;
+//             }
+//         }
+//         else{
+//             row += " ";
+//             if(j == 4){
+//                 k--;
+//             }
+//         }
+//     }
+//     console.log(row);
+// }
+
+// Elevnth Star pattern
+let k = 0;
+for(let i = 1; i <= 7; i++){
     let row = "";
-    let k = 1;
+    if(i<=4){
+        k++;
+    } else{
+        k--;
+    }
     for(let j = 1; j <= 7; j++){
-        if(j <= 5-i || j >= 3+i){
-            row += alphabets[k-1];
-            if(j<4){
-                k++;
-            } else{
-                k--;
-            }
-        }
-        else{
+        if(j >= 5-k && j <= 3+k){
+            row += "*";
+        } else{
             row += " ";
-            if(j == 4){
-                k--;
-            }
         }
+
     }
     console.log(row);
 }
