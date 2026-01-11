ffac9e4ac4e5415e72820e5337ccda81b8b8c915
Kalpana Air <ayerkasak@gmail.com>
2026-01-11 14:04:22 +0545

    Fourteenth Star Pattern

--- DOM Revision/star_pattern.js
+++ DOM Revision/star_pattern.js
@@ -257,15 +257,46 @@
     
 
 // 13th star pattern
+// *******
+//  ***** 
+//   ***
+//    *
+
+
+// for(let i = 1; i <= 4; i++){
+//     let row = "";
+//     for(let j = 1; j <= 7; j++){
+//         if(j >= i && j<= 8-i){
+//             row += "*";
+//         }else{
+//             row += " ";
+//         }
+//     }
+//     console.log(row);
+// }
+
+
+// 14th star pattern
+
+//    1   
+//   232  
+//  34543
+// 4567654
 
 for(let i = 1; i <= 4; i++){
     let row = "";
+    let k = i;
     for(let j = 1; j <= 7; j++){
-        if(j >= i && j<= 8-i){
-            row += "*";
+        if(j >= 5 - i && j <= 3 + i){
+            row += k; 
+            if(j<4){
+                k++;
+            }else{
+                k--;
+            }
         }else{
             row += " ";
         }
     }
     console.log(row);
-}
+} 
