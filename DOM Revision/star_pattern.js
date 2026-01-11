fcaa4336a0109f16d96779c2352c5390ec584ede
Kalpana Air <ayerkasak@gmail.com>
2026-01-11 13:37:55 +0545

    Twelveth Star Pattern

--- DOM Revision/star_pattern.js
+++ DOM Revision/star_pattern.js
@@ -208,21 +208,42 @@
 //    *
 
 
+// let k = 0;
+// for(let i = 1; i <= 7; i++){
+//     let row = "";
+//     if(i<=4){
+//         k++;
+//     } else{
+//         k--;
+//     }
+//     for(let j = 1; j <= 7; j++){
+//         if(j >= 5-k && j <= 3+k){
+//             row += "*";
+//         } else{
+//             row += " ";
+//         }
+
+//     }
+//     console.log(row);
+// } 
+
+// Twelveth Pattern
+
 let k = 0;
 for(let i = 1; i <= 7; i++){
-    let row = "";
+    row = "";
     if(i<=4){
         k++;
-    } else{
-        k--;
+    }else{
+        k--
     }
-    for(let j = 1; j <= 7; j++){
-        if(j >= 5-k && j <= 3+k){
+    for(j=1; j<=4; j++){
+        if(j<=k){
             row += "*";
         } else{
             row += " ";
         }
-
     }
     console.log(row);
 }
+    
