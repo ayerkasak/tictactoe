9ab1ca9bd5ff9562979ff484f3a3b408f1d98681
Kalpana Air <ayerkasak@gmail.com>
2026-01-13 08:48:40 +0545

    Fifteenth Star Pattern

--- DOM Revision/star_pattern.js
+++ DOM Revision/star_pattern.js
@@ -283,20 +283,36 @@
 //  34543
 // 4567654
 
-for(let i = 1; i <= 4; i++){
+// for(let i = 1; i <= 4; i++){
+//     let row = "";
+//     let k = i;
+//     for(let j = 1; j <= 7; j++){
+//         if(j >= 5 - i && j <= 3 + i){
+//             row += k; 
+//             if(j<4){
+//                 k++;
+//             }else{
+//                 k--;
+//             }
+//         }else{
+//             row += " ";
+//         }
+//     }
+//     console.log(row);
+// }  
+
+// 15th star Pattern
+ 
+for(let i = 1; i <= 7; i++){
     let row = "";
-    let k = i;
+    let k = 7 - i;
     for(let j = 1; j <= 7; j++){
-        if(j >= 5 - i && j <= 3 + i){
-            row += k; 
-            if(j<4){
-                k++;
-            }else{
-                k--;
-            }
+        if(j <= 8 - i){
+            row += k;
+            k--;
         }else{
             row += " ";
         }
     }
     console.log(row);
-} 
+}
