d1be37c9cd406d6e3a1a87cc0c3416f6a2444048
Kalpana Air <ayerkasak@gmail.com>
2026-02-02 13:10:37 +0545

    Revision 2 for entering the text in the box and show in a list as on button click

--- DOM Revision/revision.js
+++ DOM Revision/revision.js
@@ -1,6 +1,29 @@
-const p = document.getElementById("p");
+
+// revision one changing the text and text color on click of the button
+// const p = document.getElementById("p");
+// const btn = document.getElementById("btn");
+
+
+// btn.addEventListener('click', function(){
+//     p.innerText = "Hello Revision one done as the message and color of the text is changed."
+//     p.style.color = "red"
+// })
+
+// Revision 2 for entering the text in the box and after clicking the button the text will be shown in the list
+const inputText = document.getElementById("inputText");
 const btn = document.getElementById("btn");
+const listItem = document.getElementById("listItem");
 
 btn.addEventListener('click', function(){
-    p.style.color = "red"
+
+    if(inputText.value === ""){
+        alert("Please enter the item");
+        return;
+    }
+    
+    const li = document.createElement("li");
+    li.innerText = inputText.value;
+
+    listItem.appendChild(li);
+    inputText.value = "";
 })
