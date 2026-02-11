14a13a27aa1852f1a8e869ba1a9844387c5d0a06
Kalpana Air <ayerkasak@gmail.com>
2026-02-11 14:17:38 +0545

    Armstrong Number series

---
+++ logic_building/armstrong_num.js
@@ -0,0 +1,20 @@
+const armstrongNum = [];
+
+function armstrongNumber(){
+    for(let  i = 1; i <= 1000; i++){
+    let s = 0;
+    let x = i;
+    let r;
+    while(x !== 0){
+        r = x % 10;
+        s = s + r * r * r;
+        x = parseInt(x / 10);
+    }
+        if(s === i){
+            armstrongNum.push(s)
+        }
+    }
+    return armstrongNum;
+}
+
+console.log(armstrongNumber());
