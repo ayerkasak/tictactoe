b744a5ec4c60334b1129aa50b000a998d882592e
Kalpana Air <ayerkasak@gmail.com>
2026-02-19 22:59:55 +0545

    Three basic examples of object like delete, update and create new property

--- objects/object_01.js
+++ objects/object_01.js
@@ -1,9 +1,40 @@
-let person = {
-    name: "Kasak",
-    country: "Nepal",
-    city: "Dhangadhi",
-    age: 29
+// Example 1
+// let person = {
+//     name: "Kasak",
+//     country: "Nepal",
+//     city: "Dhangadhi",
+//     age: 29
+// }
+
+// console.log(person.name);
+// console.log(person["age"]);
+
+// person.name = "Kasak The Pain";
+// console.log(person.name)
+
+// delete person.name;
+// console.log(person);
+
+// Example 2
+// let student = {
+//     name: "kasak",
+//     roll: 29,
+//     grade: 10,
+// }
+
+// console.log(student["name"]);
+// student.grade = 9;
+// student.school = "Kanchanjunga Secondary School";
+
+// console.log(student);
+
+// Example 3
+let car = {
+    brand:"Mahindra",
+    model:"Thar",
+    year:2025
 }
 
-console.log(person.name);
-console.log(person["age"]);
+console.log(car.model);
+car.year = 2026;
+console.log(car);
