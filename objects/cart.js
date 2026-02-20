326f4e14e2b8cf4daaa80baa635c5205ca3af6af
Kalpana Air <ayerkasak@gmail.com>
2026-02-20 12:11:14 +0545

    Add new item to the cart

--- objects/cart.js
+++ objects/cart.js
@@ -1,3 +1,14 @@
 const cart = [
-    {name:"Colgate", description:""}   
+    {itemname:"Colgate", description:"kya aapke toothpaste mein namak hai", quantity: 2},
+    {itemname:"Pepsodent ", description:"cavity protection", quantity: 3},
+    {itemname:"Sensodyne", description:"sensitivity se chhutkara", quantity: 1}   
 ]
+
+function addItem(itemName, description, quantity){
+    cart.push({itemName, description, quantity})
+}
+
+addItem("Lux", "Mehkti huyi khushbu", 5);
+
+console.log(cart);
+
