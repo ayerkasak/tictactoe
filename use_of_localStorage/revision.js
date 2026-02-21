3bdea88f00f0681200be1d87d5aafbeb45ba2ccb
Kalpana Air <ayerkasak@gmail.com>
2026-02-21 22:56:10 +0545

    Revision of localStorage Example 3

--- use_of_localStorage/revision.js
+++ use_of_localStorage/revision.js
@@ -11,13 +11,31 @@
 // console.log(`Name: ${savedUser.name}, Age: ${savedUser.age}`);
 
 // Example 2
-let student = {
-    name: "Indu",
-    marks: 89
+// let student = {
+//     name: "Indu",
+//     marks: 89
+// }
+
+// localStorage.setItem("student", JSON.stringify(student));
+
+// let userStudent = JSON.parse(localStorage.getItem("student"));
+
+// console.log(`Student's Name: ${userStudent.name}, ${userStudent.name}'s Marks: ${userStudent.marks}`);
+
+// Example 3
+let students = localStorage.getItem("students");
+students = students === null ? [] : JSON.parse(students);
+
+
+let newStudent = {
+    name: "Hari",
+    marks: 90
+};
+
+students.push(newStudent);
+
+localStorage.setItem("stuents", JSON.stringify(students));
+
+for(let student of students){
+    console.log(`${student.name} scored ${student.marks}`);
 }
-
-localStorage.setItem("student", JSON.stringify(student));
-
-let userStudent = JSON.parse(localStorage.getItem("student"));
-
-console.log(`Student's Name: ${userStudent.name}, ${userStudent.name}'s Marks: ${userStudent.marks}`);
