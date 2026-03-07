0242afd7a5d5c237843d6d405ce5d43257112434
Kalpana Air <ayerkasak@gmail.com>
2026-03-07 20:11:47 +0545

    Calculate time difference.

--- datemethod/date01.js
+++ datemethod/date01.js
@@ -5,5 +5,14 @@
 // console.log(d.getFullYear())
 
 
-let time = Date.now();
-console.log(time);
+// let time = Date.now();
+// console.log(time);
+
+// calculate time difference
+
+let start = Date.now();
+for(let i = 0; i < 1000000; i++){}
+    let end = Date.now()
+
+    console.log(end - start);
+
