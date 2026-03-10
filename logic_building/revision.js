49b757097c9cd15bc9b0d0de8936eca0a61f42c3
Kalpana Air <ayerkasak@gmail.com>
2026-03-10 12:39:41 +0545

    logic building example 4.

--- logic_building/revision.js
+++ logic_building/revision.js
@@ -15,8 +15,18 @@
 
 // Example 3
 
-let fruits = ["apple", "banana", "mango"];
+// let fruits = ["apple", "banana", "mango"];
 
-for(let i = 0; i < fruits.length; i++){
-    console.log(fruits[i]);
-};
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// };
+
+// Example 4
+
+let numbers = [3, 7, 2, 9];
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] > 5){
+        console.log(numbers[i]);
+    }
+}
