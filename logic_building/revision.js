fa89a7106be471723ab002c926a023f981caa2a5
Kalpana Air <ayerkasak@gmail.com>
2026-03-10 12:30:29 +0545

    logic building example 2.

--- logic_building/revision.js
+++ logic_building/revision.js
@@ -1,5 +1,13 @@
-let text = "hello world";
+// Example 1 for logic building
+// let text = "hello world";
 
-let words = text.split(" ");
+// let words = text.split(" ");
 
-console.log(words);
+// console.log(words);
+
+// Example 2
+let sentence = "I love javascript";
+
+let words = sentence.split(" ");
+
+console.log(words[1]);
