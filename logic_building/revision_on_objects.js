c2190dfc5c129c823a53f6dbdadd01474dd5b3fa
Kalpana Air <ayerkasak@gmail.com>
2026-03-26 21:22:13 +0545

    Example 3: Find the element with maximum frequency.

--- logic_building/revision_on_objects.js
+++ logic_building/revision_on_objects.js
@@ -18,13 +18,38 @@
 
 // Example 2: Find the element with maximum frequency
 
-const arr = [1,2,1,3,2,1];
-let obj = {};
+// const arr = [1,2,1,3,2,1];
+// let obj = {};
 
+// for(let i = 0; i < arr.length; i++){
+//     let key = arr[i];
+//     if(obj[key]){
+//         obj[key]++
+//     }else{
+//         obj[key] = 1;
+//     }
+// }
+
+// let max = 0;
+// let result = null;
+// for(const key in obj){
+//     if(obj[key] > max){
+//         max = obj[key];
+//         result = key
+//     }
+// }
+
+// console.log(result);
+// Output: 1
+
+// Example 3: Find the element with maximum frequency
+
+const arr = [4,4,2,2,2,3,3];
+let obj = {};
 for(let i = 0; i < arr.length; i++){
     let key = arr[i];
     if(obj[key]){
-        obj[key]++
+        obj[key]++;
     }else{
         obj[key] = 1;
     }
@@ -35,9 +60,10 @@ let result = null;
 for(const key in obj){
     if(obj[key] > max){
         max = obj[key];
-        result = key
+        result = key;
     }
 }
 
 console.log(result);
 
+// Output: 2
