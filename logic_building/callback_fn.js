087dadce6dab92ea69a4763e92d7a0bccdfc9c25
Kalpana Air <ayerkasak@gmail.com>
2026-04-04 22:26:47 +0545

    filter custom function

--- logic_building/callback_fn.js
+++ logic_building/callback_fn.js
@@ -128,24 +128,41 @@
 
 // const evenNumbers = [];
 // function evenNum(nums){
-    // for(let i = 0; i < nums.length; i++){
-    //     if(nums[i] % 2 == 0){
-    //         evenNumbers.push(nums[i])
-    //     }
-    // }
+//     for(let i = 0; i < nums.length; i++){
+//         if(nums[i] % 2 == 0){
+//             evenNumbers.push(nums[i])
+//         }
+//     }
 // }
 
 // console.log(evenNumbers);
 
 
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let evenNums = [];
 
 function checkEven(n){
     if(n % 2 == 0){
+        // evenNums.push(n);
         return n
     }
+    // return evenNums
 }
 
-const evenNum = nums.filter(checkEven);
 
-console.log(evenNum);
+// const evenNum = nums.filter(checkEven);
+
+// console.log(evenNum);
+
+const myFilter = (callback, arr) => {
+    let evenNum = [];
+    for(let i = 0; i < arr.length; i++){
+        let num = callback(arr[i], i, arr);
+        if(num){
+            evenNum.push(num)
+        }
+    }
+    return evenNum;
+}
+
+console.log(myFilter(checkEven, nums))
