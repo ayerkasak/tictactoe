58c8a019dd88c4ae1ca689e37fc0ce9774cc15b2
Kalpana Air <ayerkasak@gmail.com>
2026-04-06 20:28:35 +0545

    finding numbers greater than 3 using another way.

--- logic_building/customFind.js
+++ logic_building/customFind.js
@@ -1,9 +1,29 @@
 const num = [1, 2, 3, 4, 5, 6];
-const numGreaterThan3 = [];
-for(let i = 0; i < num.length; i++){
-    if(num[i] > 3){
-        numGreaterThan3.push(num[i])
+// const numGreaterThan3 = [];
+// for(let i = 0; i < num.length; i++){
+//     if(num[i] > 3){
+//         numGreaterThan3.push(num[i])
+//     }
+// }
+
+// console.log(numGreaterThan3);
+
+function findnumGreaterThan3(n){
+    if(n > 3){
+        return n;
     }
 }
 
-console.log(numGreaterThan3);
+const numGreaterThan3 = [];
+
+function findNum(callback){
+    for(let i = 0; i < num.length; i++){
+        let n = callback(num[i]);
+        if(n){
+            numGreaterThan3.push(n);
+        }
+    }
+    return numGreaterThan3;
+}
+
+console.log(findNum(findnumGreaterThan3));
