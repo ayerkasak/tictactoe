4bc04f2e85e3f7d52b78c22839f8a626683a93b2
Kalpana Air <ayerkasak@gmail.com>
2026-04-17 14:16:53 +0545

    Revision 2: Find Sum of all elements.

--- logic_building/arrayrevision.js
+++ logic_building/arrayrevision.js
@@ -1,14 +1,28 @@
 // Revision 1: Find the largest number in the array.
-const arr = [4, 9, 2, 7, 1];
+// const arr = [4, 9, 2, 7, 1];
 
-function findingLargestNumber(){
-    let max = 0;
+// function findingLargestNumber(){
+//     let max = 0;
+//     for(let i = 0; i < arr.length; i++){
+//        if(i > max){
+//         max = arr[i];
+//        }
+//     }
+//     return max
+// }
+
+// console.log(findingLargestNumber());
+
+
+// Revision 2: Find sum of all elements
+const arr = [2,4,6,8];
+
+function sumOfAllElements(){
+    let sum = 0;
     for(let i = 0; i < arr.length; i++){
-       if(i > max){
-        max = arr[i];
-       }
+        sum += arr[i]
     }
-    return max
+    return sum
 }
 
-console.log(findingLargestNumber());
+console.log(sumOfAllElements());
