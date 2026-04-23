b0dec5bb41d195cf27c242db5031e4121ece9373
Kalpana Air <ayerkasak@gmail.com>
2026-04-23 21:18:47 +0545

    learning substring search using .startsWith().

--- Substring Search/includes.js
+++ Substring Search/includes.js
@@ -1,3 +1,9 @@
 let name = "John Smith";
 
-console.log(name.includes("John"))
+// console.log(name.includes("John"))
+
+// console.log(name.includes("Smith"));
+
+// console.log(name.includes("oh"));
+
+console.log(name.includes("Mike"));
---
+++ Substring Search/startsWith.js
@@ -0,0 +1,4 @@
+let city = "Kathmandu";
+
+// console.log(city.startsWith("Kath"))
+console.log(city.startsWith("mandu"))
