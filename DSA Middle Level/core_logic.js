7d549d6f3443ceee8ca5eddb2e72886d96bb59f0
Kalpana Air <ayerkasak@gmail.com>
2026-04-27 15:48:21 +0545

    return the indeces of the array whose sum is 9.

--- DSA Middle Level/core_logic.js
+++ DSA Middle Level/core_logic.js
@@ -1,17 +1,32 @@
 // Question 1: Find two numbers whose sum = 9
-const arr = [2, 7, 11, 15];
-let sum = []
+// const arr = [2, 7, 11, 15];
+// let sum = []
 
-function sumOfTwoNumbers(){
-    for(let i = 0 ; i < arr.length; i++){
-        for(let j = i + 1; j < arr.length; j++){
-            if(arr[i] + arr[j] == 9){
-                sum.push(arr[i]);
-                sum.push(arr[j]);
-            }
+// function sumOfTwoNumbers(){
+//     for(let i = 0 ; i < arr.length; i++){
+//         for(let j = i + 1; j < arr.length; j++){
+//             if(arr[i] + arr[j] == 9){
+//                 sum.push(arr[i]);
+//                 sum.push(arr[j]);
+//             }
+//         }
+//     }
+//     return sum;
+// }
+
+// console.log(sumOfTwoNumbers());
+
+// Question 1: Return the indeces whose sum is 9
+const arr = [2, 7, 11, 15];
+
+
+for(let i = 0; i < arr.length; i++){
+    let index = [];
+    for(j = i + 1; j < arr.length; j++){
+        if(arr[i] + arr[j] == 9){
+           console.log([i, j])
         }
     }
-    return sum;
+    
 }
 
-console.log(sumOfTwoNumbers());
