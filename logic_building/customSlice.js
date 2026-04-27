38bcb6feb10733b92770ee7f54501eb30808f8e1
Kalpana Air <ayerkasak@gmail.com>
2026-04-27 16:15:53 +0545

    custom slice function.

--- logic_building/customSlice.js
+++ logic_building/customSlice.js
@@ -1,3 +1,18 @@
 const fruits = ['apple', 'banana', 'kiwi', 'orange', 'strawberry'];
-const favFruits = fruits.slice(1, 4);
-console.log(favFruits)
+// const favFruits = fruits.slice(1, 4);
+// console.log(favFruits)
+
+
+const mySlice = (arr) => {
+    let startingIndex = 2;
+    let endingIndex = 4
+    let newArray = [];
+    for(let i = startingIndex; i < arr.length; i++){
+        if(i < endingIndex){
+            newArray.push(arr[i])
+        }
+    }
+    return newArray;
+}
+
+console.log(mySlice(fruits));
