640957868340eb3c736904b78ec8545facc2d080
Kalpana Air <ayerkasak@gmail.com>
2026-04-28 15:23:00 +0545

    added data into the students table.

--- IndexedDB/CRUD on IndexedDB/script.js
+++ IndexedDB/CRUD on IndexedDB/script.js
@@ -1,23 +1,41 @@
 // let db;
 
-let request = indexedDB.open("StudentDB", 2);
+let request = indexedDB.open("StudentDB", 1);
 // console.log(request);
 
 
-request.onsuccess = (e) => {
-    let db = event.target.result;
-    console.log("Success!")
-}
+// request.onsuccess = (e) => {
+//     let db = event.target.result;
+//     console.log("Success!")
+// }
+
+// Adding a data into the table
 
 request.onupgradeneeded = (e) => {
     let db = event.target.result;
     
     db.createObjectStore("Students", {
             keyPath: "id",
-            unique: true,
-            autoincrement: true
+            autoIncrement: true
         }
     );
 
     console.log("Students store created!")
+}
+
+
+request.onsuccess = (e) =>{
+    let db = event.target.result;
+
+    let transaction = db.transaction("Students", "readwrite");
+    let store = transaction.objectStore("Students");
+
+    let addRequest = store.add({
+        name: "Kasak",
+        address: "Dhangadhi"
+    });
+
+    addRequest.onsuccess = (e) => {
+        console.log("Student added successfully!")
+    }
 }
