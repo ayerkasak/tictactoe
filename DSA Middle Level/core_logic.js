d8f3180dc5e83e1d3f825ccf18986d78365eeb2a
Kalpana Air <ayerkasak@gmail.com>
2026-05-04 20:10:20 +0545

    DSA Mid-Level Question 3.

--- DSA Middle Level/core_logic.js
+++ DSA Middle Level/core_logic.js
@@ -16,18 +16,34 @@
 
 // console.log(sumOfTwoNumbers());
 
-// Question 1: Return the indeces whose sum is 9
-const arr = [2, 7, 11, 15];
+// Question 2: Return the indeces whose sum is 9
+// const arr = [2, 7, 11, 15];
 
 
-function sumOFTwo(){
-    for(let i = 0; i < arr.length; i++){
-        for(let j = i + 1; j < arr.length; j++){
-            if(arr[i] + arr[j] == 9){
-                return [i, j];
+// function sumOFTwo(){
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = i + 1; j < arr.length; j++){
+//             if(arr[i] + arr[j] == 9){
+//                 return [i, j];
+//             }
+//         }
+//     }
+// }
+
+// console.log(sumOFTwo())
+
+// Question 3 [3,2,4] target = 6
+
+const nums = [3, 2, 4];
+
+function targetSix(){
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if (nums[i] + nums[j] == 6){
+                return [i, j]
             }
         }
     }
 }
 
-console.log(sumOFTwo())
+console.log(targetSix());
