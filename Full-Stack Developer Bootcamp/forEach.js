f2b9b86f3481c9090778e330b6172b75372f1da1
Kalpana Air <ayerkasak@gmail.com>
2026-08-01 12:55:58 +0545

    Exercise 2

--- Full-Stack Developer Bootcamp/forEach.js
+++ Full-Stack Developer Bootcamp/forEach.js
@@ -4,3 +4,10 @@ function numbers(arr){
     });
 }
 numbers([1,2,3,4,5])
+
+function friends(names){
+    names.forEach((name) => {
+        console.log(`Hello ${name}`)
+    })
+}
+friends(["Ram", "Hari", "Sita"]);
