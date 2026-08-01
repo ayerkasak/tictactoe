af4083714f5ca237b31014aff6c0105a21afb364
Kalpana Air <ayerkasak@gmail.com>
2026-08-01 09:47:20 +0545

    foreach method

--- Full-Stack Developer Bootcamp/script.js
+++ Full-Stack Developer Bootcamp/script.js
@@ -1,3 +1,8 @@
-const name = "Kalpana";
-const age = 28; 
-console.log(`My name is ${name} and I am ${age} years old.`);
+// const name = "Kalpana";
+// const age = 28; 
+// console.log(`My name is ${name} and I am ${age} years old.`);
+
+// foreach
+const prices = [100, 250, 500];
+
+prices.forEach((price) => console.log(price));
