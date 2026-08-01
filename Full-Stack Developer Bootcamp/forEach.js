afe5c8adcfa7c9567c64ee1c2d46b8c020b035c7
Kalpana Air <ayerkasak@gmail.com>
2026-08-01 13:20:20 +0545

    Challenge 1

--- Full-Stack Developer Bootcamp/forEach.js
+++ Full-Stack Developer Bootcamp/forEach.js
@@ -1,20 +1,28 @@
-function numbers(arr){
-    arr.forEach(num => {
-        console.log(num)        
+// function numbers(arr){
+//     arr.forEach(num => {
+//         console.log(num)        
+//     });
+// }
+// numbers([1,2,3,4,5])
+
+// function friends(names){
+//     names.forEach((name) => {
+//         console.log(`Hello ${name}`)
+//     })
+// }
+// friends(["Ram", "Hari", "Sita"]);
+
+// function citiesToVisit(cities){
+//      cities.forEach((city) => {
+//         console.log(`I want to visit ${city}.`)
+//      })
+// }
+// citiesToVisit(["Kathmandu", "Pokhara", "Bhaktapur"])
+
+function expenses(arr){
+    arr.forEach(expense => {
+        console.log(`Expense: ${expense}`)
+ 
     });
 }
-numbers([1,2,3,4,5])
-
-function friends(names){
-    names.forEach((name) => {
-        console.log(`Hello ${name}`)
-    })
-}
-friends(["Ram", "Hari", "Sita"]);
-
-function citiesToVisit(cities){
-     cities.forEach((city) => {
-        console.log(`I want to visit ${city}.`)
-     })
-}
-citiesToVisit(["Kathmandu", "Pokhara", "Bhaktapur"])
+expenses([1200, 450, 800, 2500, 300])
