8020530507b97e055814d8b35eb1df73a8a059df
Kalpana Air <ayerkasak@gmail.com>
2026-08-05 19:44:55 +0545

    map() Exercise 2

--- Full-Stack Developer Bootcamp/map.js
+++ Full-Stack Developer Bootcamp/map.js
@@ -1,5 +1,15 @@
-function multipleOfThree(numbers){
-    const newNumbers = numbers.map((n) => n * 3)
-     return newNumbers
+// function multipleOfThree(numbers){
+//     const newNumbers = numbers.map((n) => n * 3)
+//      return newNumbers
+// }
+// console.log(multipleOfThree([2,4,6,8]))
+
+function uppercase(arr){
+    const newArr = arr.map((item) =>{ 
+      return  item.toUpperCase()})
+    
+    return newArr;
+
 }
-console.log(multipleOfThree([2,4,6,8]))
+console.log(uppercase(["ram", "hari", "sita"]))
+
