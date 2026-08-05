bd481af26e47523b0971103e4850105f34146525
Kalpana Air <ayerkasak@gmail.com>
2026-08-05 22:08:24 +0545

    filter() Exercise 2

--- Full-Stack Developer Bootcamp/filter_and_find.js
+++ Full-Stack Developer Bootcamp/filter_and_find.js
@@ -4,11 +4,24 @@
 // }
 // console.log(greaterThan20([10, 25, 30, 15, 50]))
 
-function findHari(arr){
-    const name = arr.find((name) => name === "Hari")
-    return name;
+// function findHari(arr){
+//     const name = arr.find((name) => name === "Hari")
+//     return name;
+// }
+// console.log(findHari(["Ram",
+//     "Hari",
+//     "Sita",
+//     "Gita"]))
+
+const students = [
+    { name: "Ram", marks: 45 },
+    { name: "Hari", marks: 78 },
+    { name: "Sita", marks: 90 },
+    { name: "Gita", marks: 40 }
+];
+
+function findPassedStudents(arr){
+    const passedStudents = arr.filter((item) => item.marks >= 50)
+    return passedStudents;
 }
-console.log(findHari(["Ram",
-    "Hari",
-    "Sita",
-    "Gita"]))
+console.log(findPassedStudents(students))
