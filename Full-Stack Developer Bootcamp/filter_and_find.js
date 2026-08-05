0e400e3f58f54d2e88ed8d687195b2cd84a6b9ab
Kalpana Air <ayerkasak@gmail.com>
2026-08-05 22:05:28 +0545

    find() Exercise 1

--- Full-Stack Developer Bootcamp/filter_and_find.js
+++ Full-Stack Developer Bootcamp/filter_and_find.js
@@ -1,5 +1,14 @@
-function greaterThan20(arr){
-    const greaterNumbers = arr.filter((num) => num > 20);
-    return greaterNumbers
+// function greaterThan20(arr){
+//     const greaterNumbers = arr.filter((num) => num > 20);
+//     return greaterNumbers
+// }
+// console.log(greaterThan20([10, 25, 30, 15, 50]))
+
+function findHari(arr){
+    const name = arr.find((name) => name === "Hari")
+    return name;
 }
-console.log(greaterThan20([10, 25, 30, 15, 50]))
+console.log(findHari(["Ram",
+    "Hari",
+    "Sita",
+    "Gita"]))
