72764f5fc00ac4ba4d9a3a595506088e8443d837
Kalpana Air <ayerkasak@gmail.com>
2026-08-06 18:58:16 +0545

    every() exercise1.

--- Full-Stack Developer Bootcamp/some_every.js
+++ Full-Stack Developer Bootcamp/some_every.js
@@ -1,5 +1,12 @@
-const marks = [80, 65, 90, 45];
+// const marks = [80, 65, 90, 45];
 
-const hasFailed = marks.some(mark => mark < 50);
+// const hasFailed = marks.some(mark => mark < 50);
 
-console.log(hasFailed);
+// console.log(hasFailed);
+
+function checkPasswordLength(arr){
+    const passwordLength = arr.every((pw) => pw.length >= 8)
+    return passwordLength
+}
+console.log(checkPasswordLength(["asdghcvs", "agjhasiuhga", "asgkajsgilag"]))
+
