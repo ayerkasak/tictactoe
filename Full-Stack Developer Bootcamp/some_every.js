6079449037acd213d075462072b36179291157f4
Kalpana Air <ayerkasak@gmail.com>
2026-08-06 19:01:29 +0545

    some() exercise1.

--- Full-Stack Developer Bootcamp/some_every.js
+++ Full-Stack Developer Bootcamp/some_every.js
@@ -4,9 +4,15 @@
 
 // console.log(hasFailed);
 
-function checkPasswordLength(arr){
-    const passwordLength = arr.every((pw) => pw.length >= 8)
-    return passwordLength
-}
-console.log(checkPasswordLength(["asdghcvs", "agjhasiuhga", "asgkajsgilag"]))
+// function checkPasswordLength(arr){
+//     const passwordLength = arr.every((pw) => pw.length >= 8)
+//     return passwordLength
+// }
+// console.log(checkPasswordLength(["asdghcvs", "agjhasiuhga", "asgkajsgilag"]))
 
+
+function olderPerson(arr){
+    const personOlderOrEighteen = arr.some((age) => age >= 18);
+    return personOlderOrEighteen
+}
+console.log(olderPerson([15, 18, 22, 30, 16]))
