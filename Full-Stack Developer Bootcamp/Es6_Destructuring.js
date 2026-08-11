377c246f42245a7d8f15ceebcdbf0084833fca96
Kalpana Air <ayerkasak@gmail.com>
2026-08-11 21:13:03 +0545

    ES6 Destructuring Exercise 1

--- Full-Stack Developer Bootcamp/Es6_Destructuring.js
+++ Full-Stack Developer Bootcamp/Es6_Destructuring.js
@@ -1,7 +1,14 @@
-const person = ["Ram", 20, "BCA"];
+// const person = ["Ram", 20, "BCA"];
 
-const [name, age, faculty] = person;
+// const [name, age, faculty] = person;
 
-console.log(name);
-console.log(age);
-console.log(faculty);
+// console.log(name);
+// console.log(age);
+// console.log(faculty);
+
+const colors = ["red", "green", "blue"];
+
+const [firstColor, secondColor, thirdColor] = colors
+console.log(firstColor)
+console.log(secondColor)
+console.log(thirdColor)
